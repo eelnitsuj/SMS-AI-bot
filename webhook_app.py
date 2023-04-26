bc1649620149172697d157feda97560a71c8371e
eelnitsuj <jdharwee@gmail.com>
2023-04-26 16:46:54 -0700

    filter bonsai

--- webhook_app.py
+++ webhook_app.py
@@ -13,17 +13,22 @@ def webhook():
         # Grab message body from text
         event_data = payload.get('event_data')
         message = event_data.get('body') if event_data else None
-        #Grab phone number to reply
-        phone_number = event_data.get('from_number') if event_data else None
-        print(message)
-        print(phone_number)
-        # Send the email message to OPENAI's API
-        response_text = generate_response(message)
-        print(response_text)
-        #Send Openai's response back to postscript
-        send_text(phone_number, response_text)
 
-        return jsonify({'success': True}), 200
+         # Only process messages starting with "Bonsai"
+        if message and message.lower().startswith("Bonsai"):
+            # Grab phone number to reply
+            phone_number = event_data.get('from_number') if event_data else None
+            print(message)
+            print(phone_number)
+            # Send the email message to OPENAI's API
+            response_text = generate_response(message)
+            print(response_text)
+            # Send Openai's response back to postscript
+            send_text(phone_number, response_text)
+
+            return jsonify({'success': True}), 200
+        else:
+            return jsonify({'success': False, 'message': 'Ignored non-Bonsai message'}), 200
 
 def generate_response(text):
     openai_api_key = os.environ['openai_api_key']
@@ -35,7 +40,7 @@ def generate_response(text):
                 "model": "gpt-3.5-turbo",
                 "messages": [{
                     "role": "system",
-                    "content": "You're the designated drinking buddy wingman. Avoid liability and giving medical advice and never encourage bad behaviour. Use Gen-Z language and tone. Be weary of trolls. Answer things only related to partying/alcohol/nightlife. For hangovers, suggest DHM, ginger, milk thistle, prickly pear. Ensure responses under 100 tokens"},
+                    "content": "You're a Gen-Z drink buddy wingman from NYC. Use familiar lingo, avoid liability, and dodge medical topics. Be cautious of trolls. Focus on partying/nightlife without encouraging bad behavior. Suggest DHM, ginger, milk thistle, and prickly pear for hangovers. Keep responses under 100 tokens and don't ask questions"},
                              {"role": "user", "content": text}],
                 "temperature":0.2,
                 "max_tokens":100
@@ -61,8 +66,6 @@ def send_text(phone_number, response_text):
     }
 
     response = requests.post(url, json=payload, headers=headers)
-
-    print(response.text)
 
     if response.status_code == 200:
         return 'Message sent successfully'

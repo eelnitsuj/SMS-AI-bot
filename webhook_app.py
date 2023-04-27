f454c56d781064b3f50d5125fd6acc59f99fbe0d
eelnitsuj <jdharwee@gmail.com>
2023-04-26 18:31:56 -0700

    filter bonsai

--- webhook_app.py
+++ webhook_app.py
@@ -17,20 +17,29 @@ def webhook():
         phone_number = event_data.get('from_number') if event_data else None
         print(message)
         print(phone_number)
-         # Only process messages starting with "Bonsai"
+# Filters
+        # Only process messages starting with "Bonsai"
         if message and message.lower().startswith("bonsai"):
+
             # Filter messages that are too long
             if len(message) > 200:
                 response_text = "Please shorten your message so I can read it!"
                 send_text(phone_number, response_text)
                 return jsonify({'success': False, 'message': 'Message too long'}), 200
+            elif moderate(message):
+                response_text = "Sorry, I can't process this message due to its content."
+                send_text(phone_number, response_text)
+                return jsonify({'success': False, 'message': 'Inappropriate content'}), 200
             else:
                 # Send the email message to OPENAI's API
                 response_text = generate_response(message)
                 print(response_text)
-                # Send Openai's response back to postscript
-                send_text(phone_number, response_text)
-                return jsonify({'success': True}), 200
+                if not moderate(response_text):
+                    # Send Openai's response back to postscript
+                    send_text(phone_number, response_text)
+                    return jsonify({'success': True}), 200
+                else:
+                    return jsonify({'success': False, 'message': 'Inappropriate response'}), 200
         else:
             return jsonify({'success': False, 'message': 'Ignored non-Bonsai message'}), 200
 
@@ -44,7 +53,7 @@ def generate_response(text):
                 "model": "gpt-3.5-turbo",
                 "messages": [{
                     "role": "system",
-                    "content": "You're Bonsai, a Gen-Z nightlife wingman sidekick. Use familiar lingo, avoid liability, and avoid medical advice. Be cautious of trolls. Only talk about partying/nightlife without encouraging bad behavior. If hangovers are part of the conversation, discretely suggest DHM, ginger, milk thistle, and prickly pear. Keep responses under 100 tokens and don't ask questions"},
+                    "content": "You're Bonsai, a Gen-Z nightlife wingman sidekick. Use familiar lingo, avoid liability/medical advice. Beware of trolls. Avoid very deep or political/macroeconomic topics. Only talk about light topics related to partying/nightlife without encouraging bad behavior. If hangovers are part of the conversation, discretely suggest DHM, ginger, milk thistle, and prickly pear. Keep responses <100 tokens, no questions"},
                              {"role": "user", "content": text}],
                 "temperature":0.2,
                 "max_tokens":100
@@ -75,6 +84,24 @@ def send_text(phone_number, response_text):
         return 'Message sent successfully'
     else:
         return f'Error sending message: {response.text}'
+
+def moderate(message):
+    openai_api_key = os.environ['openai_api_key']
+    
+    response = requests.post(
+        'https://api.openai.com/v1/moderations',
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {openai_api_key}'
+        },
+        json={
+            'input': message
+        }
+    )
+    
+    moderation_result = response.json()
+    flagged = moderation_result['results'][0]['flagged']
+    return flagged
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
